6e90eb8c1c17adb91800a5172b1b1e5e0c4c5a83
Lala <alejomaidana29@gmail.com>
2025-10-15 20:04:10 -0300

    avance tp

--- Trabajos-practicos/Tp7/proyecto/gestor_estudiantes/apps/estudiante/views.py
+++ Trabajos-practicos/Tp7/proyecto/gestor_estudiantes/apps/estudiante/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from apps.estudiante.models import Estudiante, Curso
+
+
+def index(request):
+    return render(request, 'estudiante/index.html')
+
+
+def alta_estudiante(request):
+    return render(request, 'estudiante/alta_estudiante.html')
+
+
+def listar_cursos(request):
+    cursos = Curso.objects.all()
+    return render(request, 'estudiante/lista_cursos.html', {'cursos': cursos})
