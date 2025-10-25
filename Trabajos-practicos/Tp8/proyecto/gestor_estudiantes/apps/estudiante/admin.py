d6a4a51106896d43724daad6a593545a2c7a518f
Lala <alejomaidana29@gmail.com>
2025-10-24 21:00:49 -0300

    termine tp8

--- Trabajos-practicos/Tp8/proyecto/gestor_estudiantes/apps/estudiante/admin.py
+++ Trabajos-practicos/Tp8/proyecto/gestor_estudiantes/apps/estudiante/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Estudiante, Curso
 
 # Register your models here.
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ['nombre_curso', 'cantidad_horas_curso']
+    search_fields = ('nombre_curso',) 
+
+@admin.register(Estudiante)
+class EstudianteAdmin(admin.ModelAdmin):
+    list_display = ['nombre_estudiante', 'apellido_estudiante', 'edad_estudiante', 'nota_curso_estudiante']
+    search_fields = ('nombre_estudiante',) 
+
+    
+
+
